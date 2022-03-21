342cb9cbfac35cf59f2daaee55ac4dd6a9de2e2d
claranceliberi <liberintwari@gmail.com>
2022-03-21 15:08:08 +0200

    stage 1

--- tour-go/hello.go
+++ tour-go/hello.go
@@ -5,6 +5,35 @@ import (
 	"math/rand"
 )
 
+func add(x int, y int) int {
+	return x + y
+}
+
+// multiple results
+func swap(x, y string) (string, string) {
+	return y, x
+}
+
+// naked return
+func split(sum int) (x, y int) {
+	x = sum * 4 / 9
+	y = sum - x
+	return
+}
+
+var c, python, java bool
+
 func main() {
-	fmt.Println("My favorite number is", rand.Intn(10))
+	fmt.Println("My favorite number is", rand.Intn(1000))
+	fmt.Println(add(42, 13))
+
+	a,b := swap("3", "4")
+	fmt.Println("Swap 3,4 =>",a,b)
+
+	fmt.Println("Naked Return")
+	x,y := split(17)
+	fmt.Println(x,y)
+
+	var i, j int = 1, 2 // bariables with initializers
+	fmt.Println("i is", i, "j is ", j, "and c is", c, "and python is", python, "and java is", java)
 }
