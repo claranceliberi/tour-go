b2e7953a478d800bf82f50b10e1d4640cfbfcf87
claranceliberi <liberintwari@gmail.com>
2022-04-04 11:09:15 +0200

    feat: error

--- tour-go/stringer.go
+++ tour-go/stringer.go
@@ -11,12 +11,12 @@ func (i IPAddr) String() string{
 	return fmt.Sprintf("\"%v.%v.%v.%v\"", i[0],i[1],i[2],i[3],)
 }
 
-func main() {
-	hosts := map[string]IPAddr{
-		"loopback":  {127, 0, 0, 1},
-		"googleDNS": {8, 8, 8, 8},
-	}
-	for name, ip := range hosts {
-		fmt.Printf("%v: %v\n", name, ip)
-	}
-}
+// func main() {
+// 	hosts := map[string]IPAddr{
+// 		"loopback":  {127, 0, 0, 1},
+// 		"googleDNS": {8, 8, 8, 8},
+// 	}
+// 	for name, ip := range hosts {
+// 		fmt.Printf("%v: %v\n", name, ip)
+// 	}
+// }
