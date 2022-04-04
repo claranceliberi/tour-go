958f1367d53f2a2f5d5992b420e240c4134dd259
claranceliberi <liberintwari@gmail.com>
2022-04-04 09:48:02 +0200

    feat: stringer

--- tour-go/interfaces.go
+++ tour-go/interfaces.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	// "fmt"
 	"math"
 )
 
@@ -9,16 +9,16 @@ type Abser interface {
 	Abs() float64
 }
 
-func main(){
-	var a Abser
-	f := MyFloat(-math.Sqrt2)
-	v := Vertex{3,4}
+// func main(){
+// 	var a Abser
+// 	f := MyFloat(-math.Sqrt2)
+// 	v := Vertex{3,4}
 
-	a = f  // a MyFloat implements Abser
-	a = &v // a *Vertex implements Abser
+// 	a = f  // a MyFloat implements Abser
+// 	a = &v // a *Vertex implements Abser
 
-	fmt.Println(a.Abs())
-}
+// 	fmt.Println(a.Abs())
+// }
 
 
 type MyFloat float64
