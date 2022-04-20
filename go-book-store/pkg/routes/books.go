1d69266dc020d0e42212c7efd53ac30e23c9afc8
claranceliberi <liberintwari@gmail.com>
2022-04-20 11:32:03 +0200

    fix: route names

--- go-book-store/pkg/routes/books.go
+++ go-book-store/pkg/routes/books.go
@@ -1,15 +1,14 @@
 package routes
 
 import (
-	"github.com/gorilla/mux"
 	"github.com/claranceliberi/go-book-store/pkg/controllers"
+	"github.com/gorilla/mux"
 )
 
-
-var RegisterBookStoreRoutes = func(router *mux.Router){
-	router.HandleFunc("books", controllers.GetBooks).Methods("GET")
-	router.HandleFunc("books", controllers.CreateBook).Methods("POST")
-	router.HandleFunc("books/{bookId}", controllers.GetBookById).Methods("GET")
-	router.HandleFunc("books/{bookId}", controllers.DeleteBook).Methods("DELETE")
-	router.HandleFunc("books/{bookId}", controllers.UpdateBook).Methods("PUT")
+var RegisterBookStoreRoutes = func(router *mux.Router) {
+	router.HandleFunc("/books", controllers.GetBooks).Methods("GET")
+	router.HandleFunc("/books", controllers.CreateBook).Methods("POST")
+	router.HandleFunc("/books/{bookId}", controllers.GetBookById).Methods("GET")
+	router.HandleFunc("/books/{bookId}", controllers.DeleteBook).Methods("DELETE")
+	router.HandleFunc("/books/{bookId}", controllers.UpdateBook).Methods("PUT")
 }
