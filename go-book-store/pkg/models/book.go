91aa27f9286f9a0eeae3d17fa3615d662c5d7fcc
claranceliberi <liberintwari@gmail.com>
2022-04-20 10:28:07 +0200

    feat: help methods

--- go-book-store/pkg/models/book.go
+++ go-book-store/pkg/models/book.go
@@ -1,23 +1,46 @@
 package models
 
 import (
-	"github.com/jinzhu/gorm"
 	"github.com/claranceliberi/go-book-store/pkg/config"
+	"github.com/jinzhu/gorm"
 )
 
+var db *gorm.DB
 
-var db* gorm.DB
-
-type Book struct{
+type Book struct {
 	gorm.Model
-	Name string `gorm:""json:"name"`
-	Author string `json:"author"`
+	Name        string `gorm:""json:"name"`
+	Author      string `json:"author"`
 	Publication string `json:"publication"`
 }
 
-
-func init(){
+func init() {
 	config.Connect()
 	db = config.GetDB()
 	db.AutoMigrate(&Book{})
+}
+
+func (b *Book) Create() *Book {
+	db.NewRecord(b)
+	db.Create(b)
+	return b
+}
+
+func GetAllBooks() []Book {
+	var books []Book
+	db.Find(&books)
+	return books
+}
+
+func GetBookById(Id int64) (*Book, *gorm.DB) {
+	var getBook Book
+
+	db := db.Where("ID=?", Id).First(&getBook)
+	return &getBook, db
+}
+
+func DeleteBook(Id int64) *Book {
+	var getBook Book
+	db.Where("ID=?", Id).Delete(&getBook)
+	return &getBook
 }
